55206b011dde0a211bb0a81ca0e04dd8b2f02dfd
Luis Heumann <luisheumann@gmail.com>
2015-12-07 16:51:32 -0500

    Corrección menu

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -209,14 +209,14 @@
             <li class="header">HEADER</li>
             <!-- Optionally, you can add icons to the links -->
             <li class="active"><a href="#"><i class="fa fa-user"></i> <span>Usuario</span></a></li>
-            <li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li>
+          <!--  <li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li>
             <li class="treeview">
               <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span> <i class="fa fa-angle-left pull-right"></i></a>
               <ul class="treeview-menu">
                 <li><a href="#">Link in level 2</a></li>
                 <li><a href="#">Link in level 2</a></li>
               </ul>
-            </li>
+            </li>-->
           </ul><!-- /.sidebar-menu -->
         </section>
         <!-- /.sidebar -->
